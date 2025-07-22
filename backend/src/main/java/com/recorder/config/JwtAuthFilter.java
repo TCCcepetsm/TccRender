f025250c060d029a05992ded615c022cb9c159e8
Janderson <jandersonjunio@hotmail.com>
2025-07-22 17:20:49 -0300

    alteraçao corsconfig polittica

--- backend/src/main/java/com/recorder/config/JwtAuthFilter.java
+++ backend/src/main/java/com/recorder/config/JwtAuthFilter.java
@@ -40,7 +40,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
 		// Configura headers CORS
 		// Configura headers CORS
-		response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5000");
+		response.setHeader("Access-Control-Allow-Origin", "https://meu-frontend-tcc.onrender.com");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 		response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
